0bc262a000659ef547b9e31f2f99c27835e58c7f
pashka <pashatett1@mail.ru>
2023-10-23 23:52:37 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+        }
+    }
+}
---
+++ WpfApp2/Window1.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Data;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text = string.Empty;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Last.Text = string.Empty;
+            Otvet.Text = string.Empty;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += 1;
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += 2;
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += 3;
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += 4;
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += 5;
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += 6;
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += 7;
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += 8;
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += 9;
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += 0;
+        }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += "/";
+        }
+
+        private void Button_Click_15(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += "*";
+        }
+
+        private void Button_Click_16(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += "-";
+        }
+
+        private void Button_Click_17(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += "+";
+        }
+
+        private void Button_Click_18(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text += ".";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Otvet.Text = Otvet.Text.Substring(0, Otvet.Text.Length - 1);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Last.Text = Otvet.Text;
+            Otvet.Text = new DataTable().Compute(Otvet.Text, null).ToString();
+        }
+    }
+}
